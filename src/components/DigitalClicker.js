09cf55f03ce9a63c00e1e1800df75b35d815f02f
Zach Frohardt <zfrohardt@gmail.com>
2021-04-27 10:31:00 -0700

    completed assignment

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0,
+        }
+    }
+    render() {
+        return (
+            <button name='click' onClick={this.clickHandler}>{this.state.timesClicked}</button>
+        );
+    }
+
+    clickHandler = () => {
+        this.setState({
+            timesClicked: this.state.timesClicked + 1,
+        })
+    }
+}
